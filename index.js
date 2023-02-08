bb76eaa3989f6d8d581beb71d5af5f135c798e44
santiago motato <santsaint6@gmail.com>
2023-02-08 17:08:52 -0500

    JavaScript Practice

---
+++ index.js
@@ -0,0 +1,11 @@
+//document.getElementById("count-el").innerText = 5
+
+//let count = 5 + 7
+//console.log(count)
+
+let firstbatch = 5
+let secondbatch = 7
+
+let count = firstbatch + secondbatch
+
+console.log(count)
