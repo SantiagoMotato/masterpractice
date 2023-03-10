05a9bebd2bda0ce782530b8ef1945e8de245e279
YipGiH <yenigomez2205@gmail.com>
2023-03-10 16:57:14 -0500

    CSS Box

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 //document.getElementById("count-el").innerText = 5
 
-//let count = 5 + 7
-//console.log(count)
+// let count = 5 + 7
+// console.log(count)
 
-let firstbatch = 5
-let secondbatch = 7
+// let firstbatch = 5
+// let secondbatch = 7
+// let count = firstbatch + secondbatch
+// console.log(count)
 
-let count = firstbatch + secondbatch
+// let myAge = 35
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
 
-console.log(count)
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+//Adding a Button
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+
+
+//in order to make function this ConvolverNode, we have to call it first: in this case "countdown"
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countdown
+
+
+function fake() {
+    console.log(42)
+} 
+fake()
